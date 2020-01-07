b0f8c6bcfd04e481e09a536c5f66c8251d58b8a4
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2020-01-06 20:32:01 -0500

    Added a visulizing dashboard under the src/components

---
+++ visualization/src/components/listitems.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import TrackChangesIcon from '@material-ui/icons/TrackChanges';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import DataUsageIcon from '@material-ui/icons/DataUsage';
+
+export const mainListItems = (
+    <div>
+        <ListItem button>
+            <ListItemIcon>
+                <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <DataUsageIcon />
+            </ListItemIcon>
+            <ListItemText primary="Current" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <BarChartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Reports" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <TrackChangesIcon />
+            </ListItemIcon>
+            <ListItemText primary="Changes" />
+        </ListItem>
+    </div>
+);
+/*
+export const secondaryListItems = (
+    <div>
+        <ListSubheader inset>Saved reports</ListSubheader>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Current month" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Last quarter" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Year-end sale" />
+        </ListItem>
+    </div>
+);
+ */
