c9033103ec30c87218200ed0d534a5ba0660546c
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2020-03-19 15:41:15 -0400

    Added front end and backend to reflect changes on map, csi data, receiver vals

--- backend/models/receiver.model.js
+++ backend/models/receiver.model.js
@@ -5,7 +5,8 @@ let receiveSchema = new Schema({
     access_point: { type: Number, default: -1},
     timestamp: { type: Date, default: Date.now },
     angle_of_arrival: { type: Number},
-    csi_frame: {type: Array, default: null}
+    csi_frame: {type: Array, default: null},
+    grid: {type: Array, default: null}
 });
 
 module.exports = mongoose.model('receiverData', receiveSchema);
---
+++ visualization/src/components/Motivation.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class Motivation extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                Hello this is Motivation
+            </div>
+        );
+    }
+}
+
+export default Motivation
