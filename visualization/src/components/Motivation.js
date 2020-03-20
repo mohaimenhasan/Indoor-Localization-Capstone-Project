280bfc6edfcb42815256ac9c412a4c9b4f907eb5
Shrey Parikh <shrey.parikh@mail.utoronto.ca>
2020-03-20 12:36:42 -0400

    Added motivation tab images and html to render said images

--- visualization/src/components/Motivation.js
+++ visualization/src/components/Motivation.js
@@ -1,4 +1,9 @@
 import React, {Component} from "react";
+import mo from './mo.png';
+import mo_cont from './mo_cont.png';
+import gps from './gps.png';
+import indoor from './indoor.png';
+import work from './work.png';
 
 class Motivation extends Component{
     constructor(props) {
@@ -8,7 +13,11 @@ class Motivation extends Component{
     render(){
         return(
             <div>
-                Hello this is Motivation
+                <img src={mo} alt="mo" />
+                <img src={mo_cont} alt="mo_cont" />
+                <img src={gps} alt="gps" />
+                <img src={indoor} alt="indoor" />               
+                <img src={work} alt="work" />
             </div>
         );
     }
